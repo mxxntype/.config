bbd4d6aec9550b453c8368395e6720a237ae8f61
astrumaureus <astrumaureus@warden>
2022-09-16 21:05:26 +0300

    switched(?) to nvchad

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,39 +1,34 @@
---[[---------------------------------------------]]--
---         init.lua - Init file of LuaVim          --
---              Author: elai, sergio               --
---                 License: GPLv3                  --
---[[---------------------------------------------]]--
+vim.defer_fn(function()
+  pcall(require, "impatient")
+end, 0)
 
-local core_modules = {
+require "core"
+require "core.options"
 
----- Core Settings ----------------------------------
- "core/plugins",           -- Plugins + Packer Config
- "core/keymaps",           -- Custom Keymaps
- "core/options",           -- General Options
+-- setup packer + plugins
+local fn = vim.fn
+local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
 
----- Plugin Configurations --------------------------
- "configs/impatient",      -- Improve Startup Time
- "configs/colorschemes",   -- All Colorschemes
- "configs/treesitter",     -- Syntax Highlighting
- "configs/completion",     -- Auto Completion
- "configs/alpha",          -- Wellcome Screen
- "configs/nvimtree",       -- File Explorer
- "configs/gitsigns",       -- Git Integration
- "configs/commentary",     -- Easily Comment Lines
- "configs/statusline",     -- Status Line
- "configs/colorizer",      -- Colors Highlighter
- "configs/neoscroll",      -- Smooth Scrolling
- "configs/toggleterm",     -- Floating Terminal
- "configs/telescope",      -- Fuzzy finder
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+  print "Cloning packer .."
+  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
 
----- Other Utilities --------------------------------
- "utils/utils",            -- Utilities
+  -- install plugins + compile their configs
+  vim.cmd "packadd packer.nvim"
+  require "plugins"
+  vim.cmd "PackerSync"
 
-}
--- Using pcall we can handle better any loading issues
-for _, module in ipairs(core_modules) do
-    local ok, err = pcall(require, module)
-    if not ok then
-      return
-    end
+  -- install binaries from mason.nvim & tsparsers
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "PackerComplete",
+    callback = function()
+      vim.cmd "bw | silent! MasonInstallAll" -- close packer window
+      require("packer").loader "nvim-treesitter"
+    end,
+  })
 end
+
+pcall(require, "custom")
+
+require("core.utils").load_mappings()
