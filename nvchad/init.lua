81b670a95efb85a318322d59b5d057578807a868
astrumaureus <astrumaureus@warden>
2022-09-18 19:16:49 +0300

    minor visual improvements & nvchad update

--- nvchad/init.lua
+++ nvchad/init.lua
@@ -1,7 +1,19 @@
-local autocmd = vim.api.nvim_create_autocmd
+-- local autocmd = vim.api.nvim_create_autocmd
+
+local opt = vim.opt
+-- local g = vim.g
 
 -- Auto resize panes when resizing nvim window
-autocmd("VimResized", {
-  pattern = "*",
-  command = "tabdo wincmd =",
-})
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+-- indenting
+opt.expandtab = true
+opt.smartindent = false
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.softtabstop = 2
+
+opt.laststatus = 3
