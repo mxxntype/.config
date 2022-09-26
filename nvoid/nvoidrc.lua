3ed45562ab75ce6438fdc3956aa233c18987292b
astrumaureus <astrumaureus@warden>
2022-09-26 20:33:33 +0300

    nVoid: set up base configuration

--- nvoid/nvoidrc.lua
+++ nvoid/nvoidrc.lua
@@ -1,11 +1,12 @@
 local M = {}
-
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
 -- UI
 M.ui = {
 	theme = "catppuccin", -- aquarium | catppuccin | classic-dark | dracula | gruvbox | nightfox | nord | onedark | radium | solarized | tokyodark | uwu
 	transparency = false,
 	statusline = {
-		style = "nvoid", -- evil | minimal | nvoid
+		style = "minimal", -- evil | minimal | nvoid
 		enabled = true,
 	},
 	bufferline = {
@@ -29,9 +30,20 @@ M.options = {
 	smart_case = true,
 	smart_indent = true,
 	swap_file = false,
+    undo_file = true,
 	backup = false,
 	show_mode = false,
 }
+
+-- MAPS
+-- navigate within insert mode
+map("i", "<C-h>", '<left>', opts)
+map("i", "<C-j>", '<down>', opts)
+map("i", "<C-k>", '<up>', opts)
+map("i", "<C-l>", '<right>', opts)
+-- toggle terminal
+map("n", "<A-i>", '<CMD>lua require("FTerm").toggle()<CR>', opts)
+map("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
 
 -- Add Treesitter langs
 M.ts_add = {} -- fish
