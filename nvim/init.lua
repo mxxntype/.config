6dca7900d1a23ea7ae47f02c1fbdd5f3fb6b0aa8
astrumaureus <astrumaureus@warden>
2022-10-07 19:55:12 +0300

    neovim: remove nVoid from repo

--- nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require("nvoid.builtin")
-require("nvoid.core.autocmd")
-require("nvoid.core.command")
-require("nvoid.core.options")
-require("nvoid.core.map")
-pcall(require, "custom.nvoidrc")
-require("nvoid.core.packer").load()
