0eda66c9ef049fead90f90353265b54ed7ed4012
astrumaureus <astrumaureus@warden>
2022-10-30 17:10:46 +0300

    swaylock: fix kb layout switching

--- swaylock/lock.sh
+++ swaylock/lock.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # change layouts back to english so i don't get locked out
-hyprctl keyword device:cooler-master-technology-inc.-gaming-mech-kb:kb_layout us
-hyprctl keyword device:at-translated-set-2-keyboard:kb_layout us
+hyprctl keyword input:kb_layout us,ru
 
 # lock the screen
 exec swaylock
