548068dde54cb94a8ef02df7c09a019560f1700e
astrumaureus <astrumaureus@warden>
2023-01-11 16:14:48 +0300

    add flags: --manual, --from-idle

--- swaylock/lock.sh
+++ swaylock/lock.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
-# pause cmus
-cmus-remote -C player-pause-playback
+common () {
+  # change layouts back to english so i don't get locked out
+  hyprctl keyword input:kb_layout us,ru
 
-# change layouts back to english so i don't get locked out
-hyprctl keyword input:kb_layout us,ru
+  # query current wallpaper
+  WALLPAPER=$(swww query | sed -ne '/^.*image: /{;s///p;q}')
 
-WALLPAPER=$(swww query | sed -ne '/^.*image: /{;s///p;q}')
-echo "$WALLPAPER"
+  # start the swaylock daemon
+  exec swaylockd --image "$HOME/.config/wallpapers/gradient/$WALLPAPER"
+}
 
-# start the swaylock daemon
-exec swaylockd --image "$HOME/.config/wallpapers/gradient/$WALLPAPER"
+if [[ "$1" = "--manual" ]] || [[ "$1" = "" ]]; then
+  echo "lock.sh: trigged manually."
+  # pause cmus
+  cmus-remote -C player-pause-playback
+fi
+
+if [[ "$1" = "--from-idle" ]]; then
+  echo "lock.sh: triggered by idle."
+fi
+
+common
